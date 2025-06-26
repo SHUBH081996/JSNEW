11fb9dbbbeb1db0482d98ed794d417bf26f5a40b
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-26 17:12:35 +0000

    conversions

--- 01_basics/Conversion.js
+++ 01_basics/Conversion.js
@@ -1,11 +1,20 @@
-let score = true
+// let score = true
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof (score));
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
 
 
