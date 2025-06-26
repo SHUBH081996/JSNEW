b731802b3b0c90861241eb9476daf770c7507dcb
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-26 17:28:33 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,31 @@
+//***********************OPERATIONS*************************** */
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+let x = 3;
+const y = x++;
+
+console.log(`value of x : ${x} and value of y : ${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`value of a : ${a} value of b ${b}`);
+
+
+
+
+
+
+
+
+
+
