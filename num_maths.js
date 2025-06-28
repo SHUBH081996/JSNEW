1abdeeec90bac9273c77a746df3601b3d7d1ac1e
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-28 09:53:51 +0000

    maths

---
+++ num_maths.js
@@ -0,0 +1,22 @@
+// const scroe = 400
+
+// const balance = new Number(200)
+
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(typeof(balance));
+
+// let newscrore = balance.toString();
+// console.log(typeof(newscrore));
+
+// const otherNumber = 23.4554
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//MATHS
+
+console.log(Math);
