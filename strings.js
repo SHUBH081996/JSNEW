ac33cc8ef81918d994bdbbd8026df7868f4e7e1e
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-28 09:29:46 +0000

    strings

---
+++ strings.js
@@ -0,0 +1,29 @@
+const name = "praful"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameNAme = new String('prafulFC')
+
+console.log(gameNAme[0])
+console.log(gameNAme.__proto__);
+
+console.log(gameNAme.length);
+console.log(gameNAme.toUpperCase());
+console.log(gameNAme.charAt(2));
+console.log(gameNAme.indexOf('p'));
+
+console.log(gameNAme.substring(2,5))
+
+const newstring = gameNAme.substring(0,4)
+console.log(newstring);
+const newslice = gameNAme.slice(3,4)
+console.log(newslice);
+
+
+
+
+
+
+
