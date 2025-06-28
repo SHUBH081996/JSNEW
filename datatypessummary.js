2edab324852f056354e56deff25f7bad45b7bc63
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-28 05:31:43 +0000

    data type summary

---
+++ datatypessummary.js
@@ -0,0 +1,31 @@
+//primitives
+
+// 7 types : string ,Number,Boolean,null,undefined,symbol,BigInt 
+
+const score  = 100
+const scoreval = 100.3
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail ;
+
+
+
+//reference or non primitive 
+
+//arrays , objects , functions 
+
+const heroes = ["ironmane","superman","hulk"]
+
+let myobj = 
+{
+    name : "praful",
+    age : 22
+}
+
+const addnumbers = function()
+{
+    console.log("hello dear")
+}
+
+
+
