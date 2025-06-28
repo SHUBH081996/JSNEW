4b663f135d95e0be031cc9e68c276ddcc9f95a5d
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-28 11:34:14 +0000

    array opeartions

---
+++ 02_basics/Arrrays.js
@@ -0,0 +1,83 @@
+// const myArr = [1,2,3,4,5,6]
+// const myarray = [1,2,3,4,5,6]
+
+// // console.log(myArr[0]);
+
+// const muHeroes = ["superman","batman"]
+
+// const myarr2 = new Array(22,332,223,4343)
+
+// // myArr.push(3332)
+// // console.log(myArr);
+
+// // myArr.pop()
+// // console.log(myArr);
+
+// // myArr.unshift(0)
+
+// // console.log(myArr);
+
+// // myArr.shift()
+// // console.log(myArr);
+
+// const newarr = myArr.join(myarray)
+// // console.log(newarr);
+
+// const newarray1 = myarray.slice(2,4)
+// console.log(myarray);
+// console.log(newarray1);
+
+
+const marvel = ["thor","ironman","spiderman"]
+const dc = ["superman",'flash',"batman"]
+
+
+
+// console.log(marvel);
+// console.log(marvel[3][2]);
+// const allheroes = marvel.concat(dc)
+// console.log(allheroes);
+
+
+const allnewheroes  = [...marvel,...dc]
+// console.log(allnewheroes);
+
+const array2 = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const realarray = array2.flat(4)
+// console.log(realarray);
+
+
+
+
+
+// console.log(Array.isArray("praful"));
+// console.log(Array.from("praful"));
+// console.log(Array.from({name : "praful"}));
+
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
