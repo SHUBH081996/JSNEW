6ed61677a9c46874845b31c491a09cc14b4647e7
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-29 10:44:19 +0000

    iifee

--- 03_Functions/Arrow.js
+++ 03_Functions/Arrow.js
@@ -1,59 +1,88 @@
-const user = 
-{
-    username : "praful",
-    price : 999,
+// // const user = 
+// // {
+// //     username : "praful",
+// //     price : 999,
 
-    welcomemessage : function(){
-        console.log(`welocme to website ${this.username}`);
+// //     welcomemessage : function(){
+// //         console.log(`welocme to website ${this.username}`);
         
-    }
+// //     }
 
-}
+// // }
 
-// user.welcomemessage()
-// user.username = "sam"
-// user.welcomemessage()
+// // // user.welcomemessage()
+// // // user.username = "sam"
+// // // user.welcomemessage()
 
 
-// console.log(this);
+// // // console.log(this);
 
-// function chai()
-// {
-//     let username = "praful"
-//     console.log(this.username);
+// // // function chai()
+// // // {
+// // //     let username = "praful"
+// // //     console.log(this.username);
     
-// }
+// // // }
 
-// chai()
-
-
-
-const chai = (value1 , value2) =>
-{
-    let username = "praful"
-console.log(this.username);
-
-}
-
-// console.log(chai());
+// // // chai()
 
 
 
-// const addtwo = (num1 , num2) =>
-// {
-//     return num1 + num2
-// }
+// // const chai = (value1 , value2) =>
+// // {
+// //     let username = "praful"
+// // console.log(this.username);
+
+// // }
+
+// // // console.log(chai());
 
 
-// const addtwo = (num1 , num2) =>  num1 + num2
 
-// const addtwo = (num1 , num2) =>  (num1 + num2)
+// // // const addtwo = (num1 , num2) =>
+// // // {
+// // //     return num1 + num2
+// // // }
 
-const addtwo = (num1 , num2) =>  ({username : "praful"})
+
+// // // const addtwo = (num1 , num2) =>  num1 + num2
+
+// // // const addtwo = (num1 , num2) =>  (num1 + num2)
+
+// // const addtwo = (num1 , num2) =>  ({username : "praful"})
 
     
 
-console.log(addtwo());
+// // console.log(addtwo());
+
+
+
+
+
+
+// //PMMEDIATELY INVOKE FUNCTIONS (IIFE)
+
+
+// //NAMED IIFE
+// (function chai()
+// {
+//     console.log("DB CONNECTED");
+    
+// })();
+
+// //(function to be written)(//function to be called)
+
+// //UNNAMED IIFE
+// ( (name) =>
+// {
+//     console.log(`DB CONNECTED TWO ${name}`);
+    
+// })("praful")
+
+
+
+
+
 
 
 
