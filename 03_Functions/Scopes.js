18fbb98b04453c532a7acb13760f6aee4ca0f363
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-29 09:54:43 +0000

    Scopes

---
+++ 03_Functions/Scopes.js
@@ -0,0 +1,33 @@
+function one()
+{
+    const username = "praful"
+
+    function two()
+    {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+   // console.log(website);
+
+    two()
+    
+}
+//////////////////////////////////////////////////
+
+
+console.log("called this " +  addone(5));
+function addone(num){
+
+    return num + 1;
+}
+
+// console.log(
+    
+//     addtwo(5));
+
+const addtwo = function(num)
+{
+    return num + 2;
+}
+   
