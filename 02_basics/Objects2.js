2802abaa0fe6606bf23c85ed3fb1497c53ad03bb
SHUBH081996 <prafulsor2011@gmail.com>
2025-06-29 09:35:22 +0000

    function2

---
+++ 02_basics/Objects2.js
@@ -0,0 +1,46 @@
+const course = 
+{
+    courseName : "javascript",
+    price : 999,
+    courseTeacher : "praful"
+}
+
+const {courseTeacher : teacher} = course 
+
+// console.log(teacher);
+
+// const navbar = ({company}) =>{
+
+// }
+
+// navbar(company = "praful")
+
+// {
+//     "name" : "praful",
+//     "courseName" : "jshindi",
+//     "price" : "999"
+// }
+
+
+
+
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03_Functions/function2.js
@@ -0,0 +1,24 @@
+// // let a = 10;
+// // const b = 20
+// // var c = 30 
+
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// var c = 308765
+
+// if(true){
+// //     let a = 10;
+// // const b = 20
+// var c = 30 
+// }
+
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
+
+
+
+
+
